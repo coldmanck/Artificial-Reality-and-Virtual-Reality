3fc32d4828053d7dc5400b3a7faa48ddef3bf972
Meng-Jiun Chiou <coldmanck@gmail.com>
2016-04-04 11:48:24 +0800

    init

---
+++ Roll A Boll/Assets/PickUpController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpController : MonoBehaviour {
+
+	public ParticleSystem smoke;
+
+	// Use this for initialization
+	void Start () {
+		smoke = GetComponent<ParticleSystem> ();
+//		smoke.Stop ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Player")) {
+			smoke.Play ();
+			this.gameObject.SetActive (false);	
+		}
+	}
+}
---
+++ Roll A Boll/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private int count;
+	private Rigidbody rb;
+
+	public float speed;
+	public Text countText, winText;
+//	public AudioClip pointSound;
+	public AudioSource audio1, audio2;
+
+	// Use this for initialization
+	void Start () {
+		audio1 = GetComponent<AudioSource>();
+		audio2 = GetComponent<AudioSource>();
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+//			GameObject s = (GameObject)Instantiate(Smoke, transform.position, Quarternion.identity);
+//			Destroy (s, 0.5);
+
+			count += 1;
+			SetCountText ();
+			audio1.Play();
+
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if(count == 8)
+			winText.text = "You Win!";
+	}
+}
